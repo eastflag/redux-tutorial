e2b5d011bcfa84b77c10e55f79364cf34634fbd1
최철환 <hwanyhee@naver.com>
2019-04-27 14:21:04 +0900

    redux 튜토리얼
    createStore 하기
    action dispatch 하기
    reducer 처리
    subscribe 하여 데이터 수신하기

--- src/index.js
+++ src/index.js
@@ -3,8 +3,44 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {combineReducers, createStore} from 'redux';
+import {Provider} from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const productReducer = (state = [], action) => {
+  return state;
+}
+
+const userReducer = (state = '', action) => {
+  switch(action.type) {
+    case 'updateUser':
+      return action.payload.user;
+    default:
+      return state;
+  }
+}
+
+const allReducer = combineReducers({
+  productReducer,
+  userReducer
+})
+
+const updateUser = {
+  type: 'updateUser',
+  payload: {
+    user: 'Tom'
+  }
+};
+
+const store = createStore(allReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+console.log(store);
+
+store.subscribe(() => console.log('subscriber: ', store.getState()));
+
+store.dispatch(updateUser);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
